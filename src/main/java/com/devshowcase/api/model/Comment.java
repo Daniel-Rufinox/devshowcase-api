fe11675f2db5f5377bc70bbcd4da3f89433e6daf
Daniel-Rufinox <danielruf1999@gmail.com>
2026-09-19 06:50:53 -0300

    Criar src/main/java/com/devshowcase/api/model/Comment.java

---
+++ src/main/java/com/devshowcase/api/model/Comment.java
@@ -0,0 +1,34 @@
+package com.devshowcase.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String author;
+    private String content;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    @JsonIgnore
+    private Project project;
+
+    public Comment() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public Project getProject() { return project; }
+    public void setProject(Project project) { this.project = project; }
+}
