87de5ad32450151899e7d8b0898c75b32f2d924b
Daniel-Rufinox <danielruf1999@gmail.com>
2026-09-19 06:56:12 -0300

    Criar src/main/java/com/devshowcase/api/repository/TechnologyRepository.java

---
+++ src/main/java/com/devshowcase/api/repository/TechnologyRepository.java
@@ -0,0 +1,9 @@
+package com.devshowcase.api.repository;
+
+import com.devshowcase.api.model.Technology;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+    Optional<Technology> findByName(String name);
+}
